26fbbaa9b28fb082e3d918b5ab2747dc12d19d40
smatch <smatch@iir.csie.ncku.edu.tw>
2013-02-17 18:41:23 +0800

    Add Comments

--- src/smatch/com/DC/ActService.java
+++ src/smatch/com/DC/ActService.java
@@ -1,10 +1,14 @@
+/*=====================================================================================*/
+/*Project : 		DataCollection App
+/*執行功能：	負責記錄Raw Data與存檔
+/*關聯檔案：	Feature.java
+/*=====================================================================================*/
 package smatch.com.DC;
 
 import static android.hardware.SensorManager.SENSOR_ACCELEROMETER;
 import static android.hardware.SensorManager.SENSOR_DELAY_FASTEST;
 import static android.hardware.SensorManager.SENSOR_DELAY_UI;
 import static android.hardware.SensorManager.SENSOR_ORIENTATION;
-
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
@@ -17,9 +21,7 @@ import java.util.Date;
 import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
-
 import smatch.com.DC.DC.MyHandler;
-
 import android.app.Service;
 import android.content.BroadcastReceiver;
 import android.content.Context;
@@ -40,29 +42,26 @@ import android.widget.Button;
 import android.widget.Toast;
 
 public class ActService extends Service implements SensorListener {
-	private final static String TAG = "Smatch";
 	private String filename = null;
 	private SensorManager sensormgr=null;
+	/*		記錄ACCELEROMETER 資料		*/
 	private List<Float> x_list = new ArrayList<Float>();
 	private List<Float> y_list = new ArrayList<Float>();
 	private List<Float> z_list = new ArrayList<Float>();
-	private List<Float> v0_list = new ArrayList<Float>();
-	private List<Float> v1_list = new ArrayList<Float>();
-	private List<Float> v2_list = new ArrayList<Float>();
 	private List<Float> Fx_list = new ArrayList<Float>();
 	private List<Float> Fy_list = new ArrayList<Float>();
 	private List<Float> Fz_list = new ArrayList<Float>();
+	/*		記錄ORIENTATION 資料		*/
+	private List<Float> v0_list = new ArrayList<Float>();
+	private List<Float> v1_list = new ArrayList<Float>();
+	private List<Float> v2_list = new ArrayList<Float>();
+	
 	float x,y,z,v0,v1,v2;
 	Timer timer;
 	MyHandler hm;
-	private int time=600;
 	SimpleDateFormat DTFormat = new SimpleDateFormat("yyyy-MM-dd-HH-mm-ss");
-	// 電池狀態
-	public int m_nStatus = 0; // 滿的？充電？
-	int batteryStatus = 0;
 
 	public ActService() {
-
 	}
 
 	@Override
@@ -74,11 +73,7 @@ public class ActService extends Service implements SensorListener {
 	@Override
 	public void onStart(Intent intent, int startId) {
 		super.onStart(intent, startId);
-		filename = intent.getExtras().getString("filename");
-		// LM = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
-		IntentFilter filter = new IntentFilter();
-		filter.addAction(Intent.ACTION_BATTERY_CHANGED);
-		
+		filename = intent.getExtras().getString("filename");		
 		hm=new MyHandler(Looper.getMainLooper());
 		start();
 		
@@ -86,13 +81,14 @@ public class ActService extends Service implements SensorListener {
 	
 	public void start(){
 		timer = new Timer(true);
+		/*		註冊Sensor服務		*/
 		sensormgr = (SensorManager)getSystemService(SENSOR_SERVICE);
     	sensormgr.registerListener(ActService.this,SENSOR_ACCELEROMETER|SENSOR_ORIENTATION,SENSOR_DELAY_FASTEST);
-    	TimerTask GetSensorData = new TimerTask() {
+    	/*		設置計時器，以20Hz的頻率收集感測器資料		*/
+		TimerTask GetSensorData = new TimerTask() {
 			public void run() {
 				Message msg =hm.obtainMessage(1);
 				hm.sendMessage(msg);
-				Log.d("Time","Send");
 			}
 		};
 		timer.scheduleAtFixedRate(GetSensorData, 50, 50);
@@ -100,11 +96,9 @@ public class ActService extends Service implements SensorListener {
 	
 	@Override
 	public void onDestroy() {
-
+		/*		結束Service前將Raw存檔		*/
 		Message msg =hm.obtainMessage(2);
 		hm.sendMessage(msg);
-
-		stopSelf();
 		super.onDestroy();
 	}
 
@@ -113,6 +107,7 @@ public class ActService extends Service implements SensorListener {
 
 	}
 
+	/*		隨時記錄Sensor各個值的改變		*/
 	@Override
 	public void onSensorChanged(int sensor, float[] values) {
 		// TODO Auto-generated method stub
@@ -187,20 +182,17 @@ public class ActService extends Service implements SensorListener {
 
 	public void onProviderDisabled(String provider) {
 		// TODO Auto-generated method stub
-		Log.d(TAG, "msg: " + "Provider Disabled");
 	}
 
 	public void onProviderEnabled(String provider) {
 		// TODO Auto-generated method stub
-		Log.d(TAG, "msg: " + "Provider enabled");
 	}
 
 	public void onStatusChanged(String provider, int status, Bundle extras) {
 		// TODO Auto-generated method stub
-		Log.d(TAG, "msg: " + "Status");
 	}
 
-
+	/*		把Raw存檔記錄		*/
 	public void write(List<Float> x, List<Float> y, List<Float> z, int id, String startTime)
     {
 		try
@@ -239,16 +231,17 @@ public class ActService extends Service implements SensorListener {
 			e.printStackTrace();
 		}
 	}
-	
+	/*		每7秒計算一次特徵，並記錄在檔案中		*/
 	public void WriteFeatureFile(List<Float> x_list,
 			List<Float> y_list, List<Float> z_list) {
+		/*	建立特徵的類別，利用特徵類別的方法計算完特徵，以陣列方式傳回	*/
 		Features features = new Features(x_list, y_list, z_list);
 		double[] featureSet = features.getFeatureSet();
-
+		
 		Date thisdate = new Date();
 		String featureDT = DTFormat.format(thisdate);
-
 		try {
+			/*		將特徵存檔		*/
 			File fileDir2 = new File("/sdcard/Smatch_DC/");
 			if (!fileDir2.exists()) {
 				fileDir2.mkdirs();
@@ -258,7 +251,6 @@ public class ActService extends Service implements SensorListener {
 			File file2 = new File(file_path2);
 			BufferedWriter bufOut = new BufferedWriter(new FileWriter(file2,
 					true));
-			// for (int i = 1; i < time.size(); i++) {
 			bufOut.append(featureDT + "\t" + featureSet[0] + "\t" + featureSet[1]
 					+ "\t" + featureSet[2] + "\t" + featureSet[3] + "\t"
 					+ featureSet[4] + "\t" + featureSet[5] + "\t"
@@ -273,11 +265,9 @@ public class ActService extends Service implements SensorListener {
 					+ featureSet[22] + "\t"
 					+ featureSet[23] + "\t" + featureSet[24] + "\t" + featureSet[25]
 					+ "\n");
-			// }
 			bufOut.close();
 		} catch (Exception e) {
 		}
-//		Toast.makeText(ActService.this, "Features saved", Toast.LENGTH_SHORT).show();
 	}
 
 }
